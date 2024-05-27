bddb005eeb0e709bded4bf94ce5ff77820b1e2c5
ykdlb <2596911668@qq.com>
2024-05-27 19:04:42 +0800

    update

--- scripts/chat/claude_chat.py
+++ scripts/chat/claude_chat.py
@@ -25,9 +25,7 @@ config = configparser.ConfigParser()
 
 # Read the config.ini file
 config.read('config.ini')
-input_file_path = "document/training_data/raw_data/raw_data_hate_002.json"
 file_template_path = "document/training_data/template.json"
-output_file_path = "utils/strer2/output/encode_data_hate_002.json"
 
 # Access the values in the configuration file
 persona_file = config['DEFAULT']['persona_file']
@@ -37,31 +35,35 @@ transit_url = config['DEFAULT']['transit_url']
 memory_length = config['DEFAULT']['memory_length']
 chat_model = config['DEFAULT']['chat_model']
 
-prompts_address = "./document/prompts/template2.txt"
-
-def save_data(content):
+def save_data(content, data_type):
     base_path = './document/training_data/raw_data/'
-    base_filename = 'raw_data_hate_'
+    base_filename = f'raw_data_{data_type}_'
+    encode_filename = f'encode_data_{data_type}_'
     file_extension = '.json'
     file_template_path = "document/training_data/template.json"
-    output_base_path = "utils/strer2/output/encode_data_hate_"
+    output_base_path = "utils/strer2/output/"
+    
     # 获取现有文件列表并找出最大的编号
     existing_files = [f for f in os.listdir(base_path) if f.startswith(base_filename) and f.endswith(file_extension)]
-    existing_numbers = [int(re.search(r'(\d{3})', f).group()) for f in existing_files]
-
+    existing_numbers = [int(re.search(r'(\d{3})', f).group()) for f in existing_files if re.search(r'(\d{3})', f)]
+    
     if existing_numbers:
         start_number = max(existing_numbers) + 1
     else:
         start_number = 1
+        
     # 循环生成文件
     file_number = str(start_number).zfill(3)  # 生成三位数的编号，例如 001, 002
+    output_filename = f'{encode_filename}{file_number}{file_extension}'
     filename = f'{base_filename}{file_number}{file_extension}'
     file_path = os.path.join(base_path, filename)
     
     with open(file_path, 'w', encoding='utf-8') as text_file:
         text_file.write(content)
-    output_file_path = os.path.join(output_base_path,file_number,file_extension)
-    encoder.encoder(file_path,file_template_path,output_file_path)
+        
+    output_file_path = os.path.join(output_base_path, output_filename)
+    
+    encoder.encoder(file_path, file_template_path, output_file_path)
     
 def create_model(modelName):
     if modelName == "custom":
@@ -71,8 +73,12 @@ def create_model(modelName):
     return model
 
 if __name__ == '__main__':
-    model = create_model("custom")
+    train_type = "affinity"
+    # hate,affinity
+    data_epoch = 10
 
+    model = create_model("custom")
+    prompts_address = f'./document/prompts/{train_type}.txt'
     if model is None:
         raise ValueError("Failed to create model. The model is None.")
     
@@ -83,7 +89,9 @@ if __name__ == '__main__':
         SystemMessage(content="根据以下描述生成一个json文件："),
         HumanMessage(content=doc),
     ]
+    for i in range(data_epoch):
+        print("data_generation:")
+        print(i+1)
+        response = model.invoke(messages)
 
-    response = model.invoke(messages)
-    save_data(response.content)
-    
+        save_data(response.content, train_type)
